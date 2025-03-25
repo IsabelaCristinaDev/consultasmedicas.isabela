e774a21d6ee264f1f3e1f93d2246729ea69fbcd3
Isabela <Isabela@gmail.com>
2025-03-24 21:10:08 -0300

    primeiro commit

--- src/main/java/com/senai/isabela/consultasmedicas/ConsultasmedicasApplication.java
+++ src/main/java/com/senai/isabela/consultasmedicas/ConsultasmedicasApplication.java
@@ -4,7 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ConsultasmedicasApplication {
+public class
+ConsultasmedicasApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ConsultasmedicasApplication.class, args);
